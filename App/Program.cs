57b89222561110951db34877f7dff3990989b800
Daniws02 <santanadanilo576@gmail.com>
2023-01-25 19:40:26 -0300

    first commit

---
+++ App/Program.cs
@@ -0,0 +1,52 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+
+    Console.Write("Numero: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    extraLongFactorials(numero);
+}
+
+    static void extraLongFactorials(int n)
+    {
+        string total = "1";
+        for (int i = 2; i <= n; i++)
+        {
+            total = Multiplo(total, i);
+        }
+
+        Console.WriteLine(total);
+    }
+    static string Multiplo(string a, int b)
+    {
+        string retorno = "";
+
+        int carry = 0;
+        
+        for (int i = a.Length - 1; i >= 0; i--)
+        {
+            int temp = (int.Parse(a[i].ToString()) * b);
+
+            temp += carry;
+
+            carry = 0;
+
+            int unitsPlace = temp % 10;
+            retorno = unitsPlace + retorno;
+
+            if (temp > 9)
+            {
+                carry = temp / 10;
+            }
+        }
+
+        if (carry != 0)
+        {
+            retorno = carry + retorno;
+        }
+
+        return retorno;
+    }
